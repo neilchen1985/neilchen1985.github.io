77c5e2f8b4a9b7334e2cb54d734ab4ffe27abc1d
Neil <neilchen1985@gmail.com>
2021-01-16 00:24:08 +0800

    no message

--- 202101_國際書展/js/script.js
+++ 202101_國際書展/js/script.js
@@ -1,7 +1,96 @@
 var played = false;
-
-window.onload = function(){
-  var triggerBtn = document.querySelector(".head_section .trigger button");
-  triggerBtn.innerText = played ? '已經玩過囉！' : '我要玩！';
-  triggerBtn.style.backgroundColor = played ? '#999999' : '#EB2E00';
+// var played = true;
+var rotating = false;
+var rotateEle = document.querySelector('.rotate img');
+var award;
+// var award = 'awards_3.png';
+var exchange = false;
+// var exchange = true;
+var triggerBtn = document.querySelector(".head_section .trigger button");
+var showAward = function(){
+  var scrollComplete = function(){
+    setTimeout(function(){
+      $(awardImg).effect('shake', 300);
+    },1000);
+  };
+  var awardImg = $('.award_section > img')[0];
+  awardImg.src = 'image/' + award;
+  $('html, body').animate({
+    scrollTop: $(".award_section").offset().top - 20
+  }, 1000, scrollComplete());
+  $('.award_section > button img')[0].src = exchange ? 'image/exchange_2.png' : 'image/exchange_1.png';
 };
+
+var rotateFn = function (awards, angles, txt){
+  rotating = !rotating;
+  $(rotateEle).stopRotate();
+  $(rotateEle).rotate({
+    angle:0,
+    animateTo:angles + 720,
+    duration:3000,
+    callback:function (){
+      console.log(txt);
+      rotating = !rotating;
+      showAward();
+    }
+  });
+  setTimeout(function(){
+    played = true;
+    triggerBtnDetect();
+  }, 3000);
+};
+var triggerBtnDetect = function(){
+    triggerBtn.disable = played ? 'true' : 'false';
+    triggerBtn.innerText = played ? '已經玩過囉！' : '我要玩！';
+    triggerBtn.style.backgroundColor = played ? '#999999' : '#EB2E00';
+};
+$('.award_section > button').click(function(){
+  exchange = true;
+  $('.award_section > button img')[0].src = exchange ? 'image/exchange_2.png' : 'image/exchange_1.png';
+  // 送紀錄
+});
+triggerBtn.addEventListener('click', function(){
+  if(rotating) return;
+  var fate = Math.floor(Math.random()*(6-0+1)+0);
+  // 0 => 特獎, 1 => 二獎, 2 => 三獎, 3 => 四獎, 4 => 知識獎, 5 => 銘謝惠顧
+  switch(fate){
+    case 0:
+      rotateFn(0, 0, '特獎');
+      award = 'awards_1.png';
+      break;
+    case 1:
+      rotateFn(1, -60, '二獎');
+      award = 'awards_2.png';
+      break;
+    case 2:
+      rotateFn(2, -120, '三獎');
+      award = 'awards_3.png';
+      break;
+    case 3:
+      rotateFn(3, -180, '四獎');
+      award = 'awards_4.png';
+      break;
+    case 4:
+      rotateFn(4, -240, '知識獎');
+      award = 'awards_5.png';
+      break;
+    case 5:
+      rotateFn(5, -300, '銘謝惠顧');
+      award = 'awards_6.png';
+      break;
+  }
+  console.log(fate);
+  // 送紀錄
+});
+
+// init
+(function(){
+  if(played){
+    showAward();
+  }else{
+    setTimeout(function(){
+      $(window).scrollTop(0);
+    }, 10); 
+  }
+  triggerBtnDetect();
+})();
